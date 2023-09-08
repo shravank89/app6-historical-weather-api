e64ec9b0ba2fdd3cf959c279d4fee5edb6169968
Shravan <newshravank25@gmail.com>
2023-09-08 17:22:13 +0530

    added all the pending URL endpoints

--- main.py
+++ main.py
@@ -23,5 +23,18 @@ def api(station_id, date):
             "temperature": temperature}
 
 
+@app.route("/api/v1/<station_id>")
+def api2(station_id):
+    df = pd.read_csv("data_small/TG_STAID" + station_id.zfill(6) + ".txt", skiprows=20, parse_dates=["    DATE"])
+    return df.to_dict(orient="records")
+
+
+@app.route("/api/v1/yearly/<station_id>/<year>")
+def api3(station_id, year):
+    df = pd.read_csv("data_small/TG_STAID" + station_id.zfill(6) + ".txt", skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    return df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
